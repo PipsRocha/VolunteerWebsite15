63538cb68a882f368ffbcacbb53fcb411c32f1d7
pipsrocha <pipa_sr@hotmail.com>
2019-03-30 00:16:25 +0000

    add code

---
+++ menu.js
@@ -0,0 +1,11 @@
+/* Set the width of the side navigation to 250px */
+function openNav() {
+    'use strict';
+    document.getElementById("mySidenav").style.width = "250px";
+}
+
+/* Set the width of the side navigation to 0 */
+function closeNav() {
+    'use strict';
+    document.getElementById("mySidenav").style.width = "0";
+}
